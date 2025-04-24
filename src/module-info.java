e5edf8ce6ac880cd677c67262b74718853c19b61
Tariq Nasser Deen <tariqnassiru@gmail.com>
2025-04-24 15:31:04 +0000

    fix: Included JUnit libraries in module-info file

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,15 @@
+
 module EmployeeManagementSystem {
     requires javafx.controls;
     requires javafx.graphics;
+    requires junit;
+
+    //requires org.junit.jupiter.engine;
+    //opens employee_management_system to junit;
+    opens database to junit;
+    opens test to junit;
+
+
 
     opens app;
 }
