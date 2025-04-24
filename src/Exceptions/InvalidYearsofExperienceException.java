03007b2d0999bead52153aeb0124b5380d7c8329
Tariq Nasser Deen <tariqnassiru@gmail.com>
2025-04-24 19:33:36 +0000

    feat(exceptions): Add new custom exceptions for user use cases

--- src/Exceptions/InvalidYearsofExperienceException.java
+++ src/Exceptions/InvalidYearsofExperienceException.java
@@ -4,7 +4,7 @@ public class InvalidYearsofExperienceException extends Exception {
     public String message;
 
     public InvalidYearsofExperienceException() {
-        super("System Exception: The employee performance data about to be inserted is invalid");
-        this.message = "System Exception: The employee performance data about to be inserted is invalid\nHint: Make sure that the value is greater than 0";
+        super("System Exception: The employee experience data about to be inserted is invalid");
+        this.message = "System Exception: The employee experience data about to be inserted is invalid\nHint: Make sure that the value is greater than 0";
     }
 }
